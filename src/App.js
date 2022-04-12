2ff1fbb6f3698591a02e16f8fa30a61f6acad495
amr303 <amr.abdulmatin@gmail.com>
2022-04-12 23:07:13 +0600

    Update user name to firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from './firebase.init';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -15,9 +15,15 @@ function App() {
 
   const [error, setError] = useState('');
 
+  const [name, setName] = useState('');
+
   const [email, setEmail] = useState('');
 
   const [password, setPassword] = useState('');
+
+  const handleNameBlur = event => {
+    setName(event.target.value);
+  }
 
   const handleEmailBlur = event => {
     setEmail(event.target.value);
@@ -42,57 +48,68 @@ function App() {
     if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
       setError('Password should contain ai least one special character.');
       return;
-
     }
 
     setValidated(true);
     setError('');
 
-    if(registered) {
+    if (registered) {
       console.log(email, password);
       signInWithEmailAndPassword(auth, email, password)
-      .then(result =>{
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error =>{
-        console.error(error);
-        setError(error.message);
-      });
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error => {
+          console.error(error);
+          setError(error.message);
+        });
 
     }
-    else{
+    else {
       createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-        setEmail('');
-        setPassword('');
-        verifyEmail();
-      })
-      .catch(error => {
-        console.error(error);
-        setError(error.message);
-      })
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setEmail('');
+          setPassword('');
+          verifyEmail();
+          setUserName();
+        })
+        .catch(error => {
+          console.error(error);
+          setError(error.message);
+        })
     }
     event.preventDefault();
   }
 
   const handlePasswordReset = () => {
     sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log('email sent');
+      })
+  }
+
+  const setUserName = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name
+    })
     .then(() => {
-      console.log('email sent');
+      console.log('updating name');
+    })
+    .catch(error => {
+      setError(error.message);
     })
   }
 
   const verifyEmail = () => {
     sendEmailVerification(auth.currentUser)
-    .then(() => {
-      console.log('Email verification');
-    })
+      .then(() => {
+        console.log('Email verification');
+      })
 
   }
-
 
   return (
     <div>
@@ -106,8 +123,16 @@ function App() {
       </form> */}
 
       <div className='registration w-50 mx-auto mt-4'>
-        <h2 className='text-primary'>Please { registered ? 'Login' : 'Register' }!!!</h2>
+        <h2 className='text-primary'>Please {registered ? 'Login' : 'Register'}!!!</h2>
         <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          { !registered &&  <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control onBlur={handleNameBlur} type="text" placeholder="Your name" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide your name.
+            </Form.Control.Feedback>
+          </Form.Group>}
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
@@ -129,6 +154,7 @@ function App() {
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered" />
           </Form.Group>
+          <p className='text-success'>{'Success'}</p>
           <p className='text-danger'>{error}</p>
           <Button onClick={handlePasswordReset} variant="link">Forget Password</Button>
           <br />
